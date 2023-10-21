93d6672b44dcebf9bac04effefb1ea792e41a449
Austin Griffith <austin@ethereum.org>
2023-10-20 18:02:59 -0600

    add balancer repo art text

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
           <div className="flex justify-center items-center gap-2 text-sm w-full">
             📝{" "}
             <a
-              href="https://github.com/BuidlGuidl/ens-stream-cohort"
+              href="https://github.com/BuidlGuidl/balancer-stream-cohort"
               target="_blank"
               rel="noreferrer"
               className="underline underline-offset-2"
